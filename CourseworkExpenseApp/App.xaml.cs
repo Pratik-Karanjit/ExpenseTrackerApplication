9ba136aecfe1ce0bdc9b072764b266e80cdf51c0
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-12-17 21:21:32 +0545

    first commit

---
+++ CourseworkExpenseApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseworkExpenseApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
